e4ca2ba465d984a11654fea374bf5bb893867a90
Franco Paredes <f.paredes.fuentes@gmail.com>
2017-10-08 19:51:11 -0300

    primer commit

---
+++ src/main/java/com/portafolio/api/service/CuentaService.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.portafolio.api.service;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import com.portafolio.api.vo.VOCuenta;
+import javax.ws.rs.POST;
+import javax.ws.rs.PathParam;
+
+/**
+ *
+ * @author FRANCO
+ */
+@Path("/cuentas")
+public class CuentaService {
+    @POST
+    @Path("/franco")
+    @Consumes({MediaType.APPLICATION_JSON})
+    @Produces({MediaType.APPLICATION_JSON})
+    public VOCuenta getCuenta(){
+        VOCuenta cuentax = new VOCuenta();
+        cuentax.setNombre("franco");
+        cuentax.setApellido("paredes");
+        
+        return cuentax;
+    }
+}
---
+++ src/main/java/com/portafolio/api/vo/VOCuenta.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.portafolio.api.vo;
+
+/**
+ *
+ * @author FRANCO
+ */
+public class VOCuenta {
+    private String nombre, apellido;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    
+    
+    
+    
+}

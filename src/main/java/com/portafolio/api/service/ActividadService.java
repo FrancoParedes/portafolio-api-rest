eac86ba380787aa45b9999b4ca4085ab9eba019a
Franco Paredes <f.paredes.fuentes@gmail.com>
2017-10-13 17:02:32 -0300

    nuevas rutas agregadas

--- src/main/java/com/portafolio/api/service/ActividadService.java
+++ src/main/java/com/portafolio/api/service/ActividadService.java
@@ -5,42 +5,36 @@
  */
 package com.portafolio.api.service;
 
-import com.portafolio.api.utils.RespuestaServidor;
+import com.portafolio.api.vo.VOActividad;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import com.portafolio.api.vo.VOCuenta;
-import java.sql.SQLException;
 import java.util.HashMap;
-import java.util.Hashtable;
-import java.util.List;
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 
 /**
  *
  * @author FRANCO
  */
-@Path("/cuentas")
-public class CuentaService {
+@Path("/actividades")
+public class ActividadService {
     
     @GET
     @Produces({MediaType.APPLICATION_JSON})
-    public Response getCuentas(){
+    public Response getActividades(){
         Response res;
         try {
-            res = Response.ok().entity(VOCuenta.all()).build();
+            res = Response.ok().entity(VOActividad.all()).build();
         }catch (Exception e) {
             Map msj = new HashMap();
             msj.put("message", e.getMessage());
@@ -51,12 +45,12 @@ public class CuentaService {
     }
     
     @GET
-    @Path("{cuenta_id}")
+    @Path("{actividad_id}")
     @Produces({MediaType.APPLICATION_JSON})
-    public Response find(@PathParam("cuenta_id") String cuenta_id){
+    public Response find(@PathParam("actividad_id") String actividad_id){
         Response res;
         try {
-            res = Response.ok().entity(VOCuenta.find(cuenta_id)).build();
+            res = Response.ok().entity(VOActividad.find(actividad_id)).build();
         } catch (Exception ex) {
             Map msj = new HashMap();
             msj.put("message", ex.getMessage());
@@ -65,54 +59,14 @@ public class CuentaService {
         
         return res;
     }
-    @PUT
-    @Path("{cuenta_id}")
-    @Consumes({MediaType.APPLICATION_FORM_URLENCODED})
-    @Produces({MediaType.APPLICATION_JSON})
-    public Response update(
-            @PathParam("cuenta_id") String cuenta_id,
-            @FormParam("nombre") String nombre,
-            @FormParam("apellido_p") String apellido_p,
-            @FormParam("apellido_m") String apellido_m,
-            @FormParam("sexo") String sexo,
-            @FormParam("email") String email,
-            @FormParam("telefono") String telefono,
-            @FormParam("celular") String celular,
-            @FormParam("rol_id") int rol_id)
-          {
-        Response res;
-        
-        
-        try {
-            VOCuenta cuenta = new VOCuenta();
-            cuenta.setId(cuenta_id);
-            cuenta.setNombre(nombre);
-            cuenta.setApellido_p(apellido_p);
-            cuenta.setApellido_m(apellido_m);
-            cuenta.setSexo(sexo.charAt(0));
-            cuenta.setEmail(email);
-            cuenta.setTelefono(telefono);
-            cuenta.setCelular(celular);
-            cuenta.setRol_id(rol_id);
-            
-            res = Response.ok().entity(cuenta.update()).build();
-            
-        } catch (Exception ex) {
-            Map msj = new HashMap();
-            msj.put("message", ex.toString());
-            res = Response.status(Response.Status.UNAUTHORIZED).entity(msj).build();
-        }
-        
-        return res;
-    }
     
     @DELETE
-    @Path("{cuenta_id}")
+    @Path("{actividad_id}")
     @Produces({MediaType.APPLICATION_JSON})
-    public Response delete(@PathParam("cuenta_id") String cuenta_id){
+    public Response delete(@PathParam("actividad_id") String actividad_id){
         Response res;
         try {
-            res = Response.ok().entity(VOCuenta.delete(cuenta_id)).build();
+            res = Response.ok().entity(VOActividad.delete(actividad_id)).build();
         } catch (Exception ex) {
             Map msj = new HashMap();
             msj.put("message", ex.getMessage());
@@ -125,23 +79,13 @@ public class CuentaService {
     @Consumes({MediaType.APPLICATION_FORM_URLENCODED})
     @Produces({MediaType.APPLICATION_JSON})
     public Response save(
-            @FormParam("rut") String rut,
-            @FormParam("nombre") String nombre,
-            @FormParam("apellido_p") String apellido_p,
-            @FormParam("apellido_m") String apellido_m,
-            @FormParam("sexo") String sexo,
-            @FormParam("email") String email,
-            @FormParam("telefono") String telefono,
-            @FormParam("celular") String celular,
-            @FormParam("password") String password,
-            @FormParam("rol_id") int rol_id)
+            @DefaultValue("") @FormParam("nombre") String nombre)
             { 
         Response res;
         try {
-            VOCuenta cuentax = new VOCuenta(rut, nombre, apellido_p, apellido_m,
-                    email, telefono, celular, password, sexo.charAt(0), rol_id);
-            cuentax.save();
-            res = Response.ok().entity(cuentax).build();
+            VOActividad actividadx = new VOActividad(nombre);
+            actividadx.save();
+            res = Response.ok().entity(actividadx).build();
         } catch (Exception ex) {
             Map msj = new HashMap();
             msj.put("message", ex.getMessage());
